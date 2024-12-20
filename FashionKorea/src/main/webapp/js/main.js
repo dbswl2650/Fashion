020b8edf8d51228809fb1478c3f999a93e0ba153
lje <lje64257@gmail.com>
2024-12-20 17:42:36 +0900

    브랜치에 먼저 저장

--- FashionKorea/src/main/webapp/js/main.js
+++ FashionKorea/src/main/webapp/js/main.js
@@ -1,17 +1,21 @@
-function insertCard() {
-	let cardHtml = 	`<div class="card" style="width: 18rem;">
-						<img src="" class="card-img-top" alt="">
+// 옷 정보 카드 읽기
+function selectCard(cloth) {
+	let cardHtml = `<div class="card" style="width: 18rem;">
+						<img src="${cloth.image}" class="card-img-top" alt="${cloth.name}">
 						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<h5 class="card-title">${cloth.name}</h5>
+							<h5 class="card-title" style="color:red">가격 : ${cloth.price}</h5>
+							<p class="card-text">색상 : ${cloth.color}<p>
+							<p class="card-text">사이즈 : ${cloth.clothesSize}<p>
+							<p class="card-text">카테고리 : ${cloth.category}<p>
+							<p class="card-text">${cloth.about}</p>
 							<a href="#" class="btn btn-primary">Go somewhere</a>
 						</div>
 					</div>`;
-	let target = document.querySelector('article');
-	console.log(target);
-	target.insertAdjacentHTML('afterbegin', cardHtml);
+	document.querySelector('article').innerHTML += cardHtml;
 }
 
+// 옷 정보 카드 삭제
 function deleteCard(rno) {
 	fetch('removeReply.do?rno=' + rno)
 	.then(result => result.json())
@@ -27,4 +31,20 @@ function deleteCard(rno) {
 	.catch(err => console.log(err));
 }
 
-let clothesList = [{}];
+
+/* fetch('main.ko')
+.then(result => result.json())
+.then(result => {
+	document.querySelector('article').innerHTML += '<div>';
+	
+	let count = 0;
+	for (let cloth of result) {
+		selectCard(cloth);
+		if (count % 5 == 4) {
+			document.querySelector('article').innerHTML += '</div><div>';
+		}
+	}
+	
+	document.querySelector('article').innerHTML += '</div>';
+})
+.catch(err => console.log(err)); */
