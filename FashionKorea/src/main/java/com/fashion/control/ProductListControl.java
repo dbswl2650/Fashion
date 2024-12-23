8db8df0be8ddf4656da2f954c9bbbf9b2594b08b
lje <lje64257@gmail.com>
2024-12-23 11:01:36 +0900

    상품목록 수정 12-23

--- FashionKorea/src/main/java/com/fashion/control/ProductListControl.java
+++ FashionKorea/src/main/java/com/fashion/control/ProductListControl.java
@@ -1,16 +1,39 @@
 package com.fashion.control;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ProductListControl implements Control {
+import com.fashion.dao.ClothesDAO;
+import com.fashion.vo.Clothes;
 
+public class ProductListControl implements Control {
+	ClothesDAO cdao = new ClothesDAO();
+	
 	@Override
 	public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("WEB-INF/html/productList.jsp").forward(request, response);
+		if (request.getMethod().equals("GET")) {
+			List<Integer> clothesNoList = cdao.selectSomeClothes();
+			List<Clothes> clothesList = new ArrayList<>();
+			
+			for (int i = 0; i < clothesNoList.size(); i++) {
+				Integer clothesNo = clothesNoList.get(i);
+				clothesList.add(cdao.selectOneClothes(clothesNo));
+			}
+			
+			request.setAttribute("clothesList", clothesList);
+			request.getRequestDispatcher("WEB-INF/html/productList.jsp").forward(request, response);
+		}
+		else if (request.getMethod().equals("POST")) {
+		}
+		else if (request.getMethod().equals("PUT")) {
+		}
+		else if (request.getMethod().equals("DELETE")) {
+		}
 	}
 }
--- FashionKorea/src/main/webapp/WEB-INF/html/productList.jsp
+++ FashionKorea/src/main/webapp/WEB-INF/html/productList.jsp
@@ -14,5 +14,37 @@
 <style>
 
 </style>
+<script>
+	let clothlist = <%=request.getAttribute("clothesList")%>;
+	
+	console.log(clothlist);
+	
+	// 옷 카드를 보여준다
+	function selectCard(cloth) {
+		let cardHtml = `<div class="card" style="width: 18rem;">
+							<img src="${cloth.image}" class="card-img-top" alt="${cloth.name}">
+							<div class="card-body">
+								<h5 class="card-title">${cloth.name}</h5>
+								<h5 class="card-title" style="color:red">가격 : ${cloth.price}</h5>
+								<p class="card-text">색상 : ${cloth.color}<p>
+								<p class="card-text">사이즈 : ${cloth.clothesSize}<p>
+								<p class="card-text">카테고리 : ${cloth.category}<p>
+								<p class="card-text">${cloth.about}</p>
+								<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>`;
+		document.querySelector('article').insertAdjacentHTML('afterbegin', cardHtml);
+	}
+	
+	document.querySelector('article').insertAdjacentHTML('afterbegin', '<div>');
+	let count = 0;
+	for (let cloth of clothlist) {
+		selectCard(cloth);
+		if (count % 5 == 4) {
+			document.querySelector('article').insertAdjacentHTML('afterbegin', '</div><div>');
+		}
+	}
+	document.querySelector('article').insertAdjacentHTML('afterbegin', '</div>');
+</script>
 
 <jsp:include page="./footer.jsp"></jsp:include>
