8c50028fd1b16b6f7df97009e42240240ddb0b29
admin <pc>
2024-12-23 13:07:10 +0900

    회원가입과 로그인

--- FashionKorea/src/main/java/com/fashion/dao/LoginDAO.java
+++ FashionKorea/src/main/java/com/fashion/dao/LoginDAO.java
@@ -1,6 +1,12 @@
 package com.fashion.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.fashion.vo.MemberVO;
 
 public class LoginDAO extends DAO {
 	public String login(String id, String pw) {
@@ -22,5 +28,32 @@ public class LoginDAO extends DAO {
 			disConnect();
 		}
 		return null;
+	}
+	
+	public boolean insertMember(MemberVO member) {
+		connect();
+		String sql = "insert into member " //
+				+ "(member_no, member_name, member_id, member_pw, member_mail, member_age, member_add, member_phone) " //
+				+ "values(member_seq.nextval, ?, ?, ?, ?, ?, ?, ?) ";
+
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, member.getMemberName());
+			psmt.setString(2, member.getMemberId());
+			psmt.setString(3, member.getMemberPw());
+			psmt.setString(4, member.getMemberMail());
+			psmt.setString(5, member.getMemberAge());
+			psmt.setString(6, member.getMemberAdd());
+			psmt.setString(7, member.getMemberPhone());
+			int r = psmt.executeUpdate(); // 쿼리실행.
+			if (r > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+		return false;
 	}
 }
