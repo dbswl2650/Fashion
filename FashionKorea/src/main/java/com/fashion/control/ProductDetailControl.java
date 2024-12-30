2aeb0026891fcf47387f1d25ba08e836c1107b76
lje <lje64257@gmail.com>
2024-12-30 16:44:19 +0900

    찜 선택/해제 기능 완성

--- FashionKorea/src/main/java/com/fashion/control/ProductDetailControl.java
+++ FashionKorea/src/main/java/com/fashion/control/ProductDetailControl.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fashion.dao.LikeItDAO;
-
 public class ProductDetailControl implements Control {
 
 	@Override
--- FashionKorea/src/main/java/com/fashion/control/ProductDetailFormControl.java
+++ FashionKorea/src/main/java/com/fashion/control/ProductDetailFormControl.java
@@ -1,9 +1,7 @@
 package com.fashion.control;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
