546ab0ff090259dfda4f4517d5c4a68ed25707bc
lje <lje64257@gmail.com>
2024-12-30 17:46:54 +0900

    이제는 찜 목록 보면 숫자만 보이는게 아니라 카드가 보이게

--- FashionKorea/src/main/java/com/fashion/control/LikeItListControl.java
+++ FashionKorea/src/main/java/com/fashion/control/LikeItListControl.java
@@ -1,13 +1,20 @@
 package com.fashion.control;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fashion.dao.CategoryDAO;
+import com.fashion.dao.ClothesDAO;
 import com.fashion.dao.LikeItDAO;
+import com.fashion.dao.OrdersItemDAO;
+import com.fashion.dao.ReviewDAO;
+import com.fashion.vo.Category;
+import com.fashion.vo.Clothes;
 
 public class LikeItListControl implements Control {
 	@Override
@@ -17,9 +24,41 @@ public class LikeItListControl implements Control {
 		int mno = member_no != null ? (int) member_no : 0;
 		
 		LikeItDAO lidao = new LikeItDAO();
-		List<Integer> likeList = lidao.selectLikes(mno);
+		ClothesDAO cldao = new ClothesDAO();
+		CategoryDAO cadao = new CategoryDAO();
+		OrdersItemDAO oidao = new OrdersItemDAO();
+		ReviewDAO rdao = new ReviewDAO();
 		
-		request.setAttribute("likeList", likeList);
+		List<Integer> clothesNoList = lidao.selectLikes(mno);
+		List<Clothes> clothesList = new ArrayList<>();
+		List<Category> categoryList = new ArrayList<>();
+		List<Boolean> isFamousList = new ArrayList<>();
+		List<Integer> reviewCntList = new ArrayList<>();
+		
+		for (int i = 0; i < clothesNoList.size(); i++) {
+			Integer clothesNo = clothesNoList.get(i);
+			Clothes clothes = cldao.selectOneClothes(clothesNo);
+			clothesList.add(clothes);
+			
+			Integer categoryNo = clothes.getCategoryNo();
+			Category category = cadao.selectOneCategory(categoryNo);
+			categoryList.add(category);
+			
+			Boolean isFamous = false;
+			if (oidao.selectClothesNoCount(clothesNo) >= 5) {	// 5가 바로 인기 임계치
+				isFamous = true;
+			}
+			isFamousList.add(isFamous);
+			
+			Integer reviewCnt = rdao.selectCountReview(clothesNo);
+			reviewCntList.add(reviewCnt);
+		}
+		
+		request.setAttribute("clothesList", clothesList);
+		request.setAttribute("categoryList", categoryList);
+		request.setAttribute("isFamousList", isFamousList);
+		request.setAttribute("reviewCntList", reviewCntList);
+
 		if(mno != 0) {
 			request.getRequestDispatcher("WEB-INF/html/likeItList.jsp").forward(request, response);
 		}
