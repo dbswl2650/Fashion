b3d65104172c1aea476348f8f2591a5f5d734f2b
PlumJE <lje64257@gmail.com>
2025-01-01 17:39:39 +0900

    스코어 넣는게 필수므로 하므로 스코어 넣는 기능 추가

--- FashionKorea/src/main/java/com/fashion/control/ReviewControl.java
+++ FashionKorea/src/main/java/com/fashion/control/ReviewControl.java
@@ -38,6 +38,8 @@ public class ReviewControl implements Control {
             String title = mr.getParameter("title");
             String comments = mr.getParameter("comments");
             String image = mr.getFilesystemName("image");
+            String score = "★";
+            score = score.repeat(Integer.parseInt(mr.getParameter("score")));
             
             int memberNo = Integer.parseInt(mr.getParameter("member_no"));
 
@@ -46,9 +48,10 @@ public class ReviewControl implements Control {
             review.setComments(comments != null ? comments : "");
             review.setMemberNo(memberNo);
             review.setImage(image);
-            review.setClothesNo(0);
+            review.setClothesNo(4);
             review.setType("게시글");
             review.setWdateDate(new java.sql.Date(System.currentTimeMillis()));
+            review.setScore(score);
 
             boolean success = rdao.insertReview(review);
             if (success) {
